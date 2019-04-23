ffb275e9e066f6cbb479cb9f8a1a77c966f28148
Jamal <architecture.315@gmail.com>
2019-04-23 22:43:38 +0800

    Adding nested resources. Adding 3 nested routes with their controllers:
    	new file:   app/Http/Controllers/AssignmentsController.php
    	new file:   app/Http/Controllers/ClassesController.php
    	new file:   app/Http/Controllers/LabsController.php
    	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('classes', 'ClassesController');
+Route::resource('classes.labs', 'LabsController');
+Route::resource('classes.labs.assignments', 'AssignmentsController');
