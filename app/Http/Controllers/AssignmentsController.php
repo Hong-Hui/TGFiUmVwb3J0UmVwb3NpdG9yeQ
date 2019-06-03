cda20e1daba02963b485fab5fc496707617dd86b
Hong-Hui <architecture.315@gmail.com>
2019-06-03 17:06:50 +0800

    removing a file added by the text editor

--- app/Http/Controllers/AssignmentsController.php
+++ app/Http/Controllers/AssignmentsController.php
@@ -16,14 +16,27 @@ class AssignmentsController extends Controller
         return view('courses.labs.assignments.index', compact('localLab', 'localAssignments'));
     }
 
-    public function create()
+    public function create($lab_id)
     {
-        //
+        $localLab = Lab::findOrFail($lab_id);
+        $localAssignments = Assignment::localAssignments($lab_id)->get();
+
+        return view('courses.labs.assignments.create', compact('localLab', 'localAssignments'));
     }
 
-    public function store(Request $request)
+    public function store(Request $request, $lab_id)
     {
-        //
+        $data = request()->validate([
+            'title' => 'required|min:10',
+            'source' => 'required',
+            'mark' => 'required',
+        ]);
+
+        $data['status'] = 'pending';
+
+        Lab::create($data);
+
+        return redirect('labs/' . $lab_id . '/assignments');
     }
 
     public function show($id)
