97ad8aca81021d0dfe4ca93e193f04148ce492c6
Hong-Hui <architecture.315@gmail.com>
2019-06-14 17:14:46 +0800

    Added spatie laravel permission package, - and created its seeders and migration.
    	modified:   app/Assignment.php
    	modified:   app/Course.php
    	modified:   app/Http/Kernel.php
    	modified:   app/Lab.php
    	modified:   app/User.php
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   config/app.php
    	new file:   config/permission.php
    	new file:   database/migrations/2019_06_14_132939_create_permission_tables.php
    	modified:   database/seeds/DatabaseSeeder.php
    	new file:   database/seeds/PermissionTableSeeder.php

--- app/Assignment.php
+++ app/Assignment.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Assignment extends Model
 {
+
     protected $guarded = [];
 
-    // Relationships
-    public function lab() {
+    public function lab()
+    {
         return $this->belongsTo(\App\Lab::class);
     }
+
 }
