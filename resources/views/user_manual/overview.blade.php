6b29f70343b24bbc5b22778bf10dcaaa8e857ba2
Hong-Hui <architecture.315@gmail.com>
2019-07-05 16:42:44 +0800

    Added overview and a license to the homepage. The License is also added to the project directory.

---
+++ resources/views/user_manual/overview.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.card')
+
+@section('card_header')
+
+<h4>overview</h4>
+
+@endsection
+
+@section('card_body')
+
+<p>welcome to the lab report repository overview.</p>
+
+<p>The lab report repository is a management system.
+    the main role of this website is to provide students with a platform where they can submit their
+    assignments and reports
+    for their teachers to review, mark, and comment on.
+
+    It provides the teachers with a platform where they can receive their students assignments to
+    mark, review, and comment on.
+    teachers will be able to create courses and invite your students to your course,
+    teachers can also create many labs under one course and set a deadline for each one of them.
+    students will start submitting assignments for each lab all while respecting the deadlines.
+    teachers can start marking and commenting on an assignment as soon as it's posted by the
+    student.</p>
+
+@endsection
